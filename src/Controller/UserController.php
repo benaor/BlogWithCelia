74969e0dc5c41ad30d55c0a9800cc64c8ff06979
benaor <benjamingirard25@gmail.com>
2021-02-14 00:05:46 +0100

    Commit

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -7,7 +7,6 @@ use App\Form\UserRegisterType;
 use Doctrine\ORM\EntityManagerInterface;
 
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
